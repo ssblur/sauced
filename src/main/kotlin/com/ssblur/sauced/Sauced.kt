768c057de7baa0ced2bab83680be07fc5c7e3ac7
Patrick Hallbick <patrick.hallbick@outlook.com>
2024-08-17 02:14:02 -0400

    Split out 2d and 3d models

--- src/main/kotlin/com/ssblur/sauced/Sauced.kt
+++ src/main/kotlin/com/ssblur/sauced/Sauced.kt
@@ -1,13 +1,17 @@
 package com.ssblur.sauced
 
 import com.ssblur.sauced.item.SauceItem
+import dev.architectury.platform.Platform
+import net.fabricmc.api.EnvType
 import net.fabricmc.api.ModInitializer
 import net.minecraft.core.Registry
+import net.minecraft.core.component.DataComponents
 import net.minecraft.core.registries.BuiltInRegistries
-import net.minecraft.core.registries.Registries
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.item.Item
 import org.slf4j.LoggerFactory
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry
+
 
 object Sauced : ModInitializer {
     val MOD_ID = "sauced"
@@ -19,6 +23,12 @@ object Sauced : ModInitializer {
         LOGGER.info("Loading Sauced for Burgered")
 
         Registry.register(BuiltInRegistries.ITEM, location("sauce"), SAUCE_ITEM)
+
+        if(Platform.getEnv() == EnvType.CLIENT) {
+            ColorProviderRegistry.ITEM.register({ stack, layer ->
+                stack.get(DataComponents.POTION_CONTENTS)?.color ?: 0xFFFFFF
+            }, SAUCE_ITEM)
+        }
 	}
 
     fun location(name: String) : ResourceLocation = ResourceLocation.fromNamespaceAndPath(MOD_ID, name)
--- src/main/kotlin/com/ssblur/sauced/item/SauceItem.kt
+++ src/main/kotlin/com/ssblur/sauced/item/SauceItem.kt
@@ -1,42 +1,50 @@
 package com.ssblur.sauced.item
 
+import com.ssblur.sauced.Sauced
 import net.minecraft.core.component.DataComponents
 import net.minecraft.network.chat.Component
 import net.minecraft.world.effect.MobEffectInstance
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.food.FoodProperties
-import net.minecraft.world.food.FoodProperties.PossibleEffect
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.TooltipFlag
 import net.minecraft.world.level.Level
 import net.wiredtomato.burgered.api.Burger
+import net.wiredtomato.burgered.api.StatusEffectEntry
 import net.wiredtomato.burgered.api.ingredient.BurgerIngredient
+import net.wiredtomato.burgered.api.ingredient.BurgerIngredientInstance
 import net.wiredtomato.burgered.api.rendering.IngredientRenderSettings
+import net.wiredtomato.burgered.api.rendering.ModelId
 import net.wiredtomato.burgered.init.BurgeredDataComponents
 import org.joml.Vector3d
 
 class SauceItem(properties: Properties) : Item(properties), BurgerIngredient {
-    override fun canBePutOn(stack: ItemStack, burger: Burger): Boolean = true
+    override fun canBePutOn(ingredientStack: ItemStack, burger: Burger, burgerStack: ItemStack): Boolean = true
 
     override fun onEat(entity: LivingEntity, world: Level, stack: ItemStack, component: FoodProperties) {
         val burger = stack.get(BurgeredDataComponents.BURGER)
-        burger?.ingredients()?.forEach { pair ->
-            if(pair.first.item is SauceItem)
-                pair.first.get(DataComponents.POTION_CONTENTS)?.allEffects?.forEach { effect ->
+        burger?.ingredients()?.forEach { ingredient ->
+            if(ingredient.ingredient is SauceItem)
+                ingredient.stack().get(DataComponents.POTION_CONTENTS)?.allEffects?.forEach { effect ->
                     entity.addEffect(MobEffectInstance(effect.effect, effect.duration, effect.amplifier))
                 }
         }
     }
 
-    override fun overSaturation(stack: ItemStack): Double = 0.0
+    override fun overSaturation(instance: BurgerIngredientInstance): Double = 0.0
 
-    override fun renderSettings(stack: ItemStack) =
-        IngredientRenderSettings.ItemModel3d(Vector3d(1.0, 1.0, 1.0), Vector3d(0.0, 0.0, 0.0), 1.0)
+    override fun renderSettings(instance: BurgerIngredientInstance): IngredientRenderSettings =
+        IngredientRenderSettings.ItemModel3d.CustomModeled(
+            Vector3d(1.0, 1.0, 1.0),
+            Vector3d(0.0, 0.0, 0.0),
+            1.0,
+            ModelId(Sauced.location("custom/burger_sauce"), "")
+        )
 
-    override fun saturation(stack: ItemStack): Int = 0
+    override fun saturation(instance: BurgerIngredientInstance): Int = 0
 
-    override fun statusEffects(stack: ItemStack): List<PossibleEffect> = listOf()
+    override fun statusEffects(instance: BurgerIngredientInstance): List<StatusEffectEntry> = listOf()
 
     override fun appendHoverText(
         itemStack: ItemStack,
