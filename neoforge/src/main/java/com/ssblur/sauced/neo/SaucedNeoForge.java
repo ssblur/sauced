31f9b13df4d73431e7e8fbaba5859174e9bc8a47
TheEnderCore <theendercore@gmail.com>
2025-05-10 00:19:23 +0300

    - fixed neo

--- neoforge/src/main/java/com/ssblur/sauced/neo/SaucedNeoForge.java
+++ neoforge/src/main/java/com/ssblur/sauced/neo/SaucedNeoForge.java
@@ -1,5 +1,6 @@
-package com.ssblur.sauced;
+package com.ssblur.sauced.neo;
 
+import com.ssblur.sauced.Sauced;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
